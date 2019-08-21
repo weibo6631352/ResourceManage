ffbbf53260efa29dc97c2c06411511181f336a77
weibo6631352 <838524022@qq.com>
2019-08-21 13:33:16 +0800

    1，还是有随机崩溃。

--- 车辙沉陷拥包检测/main.cpp
+++ 车辙沉陷拥包检测/main.cpp
@@ -4,6 +4,7 @@
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/console/print.h>
 
 
 #include "pctio.h"
@@ -20,19 +21,20 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	QDialog w;
-
+	pcl::console::setVerbosityLevel(pcl::console::L_DEBUG);
 	Setting::ins().input_file_ = "C:/Users/wang/Desktop/190511_014154_60.las";
 	Setting::ins().outdir_ = "C:/Users/wang/Desktop/";
 	Setting::ins().zone = 51;
 	Setting::ins().southhemi = false;
-	Setting::ins().sample_.gridSize = 0.08;
-	Setting::ins().gridProcess_.gridSize = 0.1; 
-	Setting::ins().liQunDian_.radius = 2; 
+	Setting::ins().sample_.gridSize = 0.1;    // 影响提取地面速度，不能小于网格尺寸
+	Setting::ins().gridProcess_.gridSize = 0.1;
+	Setting::ins().liQunDian_.radius = 2;
 	Setting::ins().liQunDian_.count = (0.7*Setting::ins().liQunDian_.radius *Setting::ins().liQunDian_.radius*3.14) / (Setting::ins().sample_.gridSize * Setting::ins().sample_.gridSize);
-	Setting::ins().groundExact_.minlDistance = 0.05;
-	Setting::ins().groundExact_.maxlDistance = 0.3;
-	Setting::ins().groundExact_.slope = 0.7;
+	Setting::ins().groundExact_.minlDistance = 0.5;
+	Setting::ins().groundExact_.maxlDistance = 0.5;
+	Setting::ins().groundExact_.slope = 1.0;
 	Setting::ins().groundExact_.windowsize = 2;
+
 
 	std::string groundLas = Setting::ins().outdir_ + "ground_.las";
 
@@ -41,29 +43,33 @@ int main(int argc, char *argv[])
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr ground_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr grid_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr binghai_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+	pcl::PointIndicesPtr cloud_indices(new pcl::PointIndices);
+	pcl::PointIndicesPtr ground_indices(new pcl::PointIndices);
+
 	pct::io::Load_las_off(src_cloud, Setting::ins().input_file_);
 
 	std::cout << "start Sample..." << src_cloud->size() << std::endl;
 	Common::Sample(*src_cloud, Setting::ins().sample_.gridSize);
 	std::cout << "end Sample." << src_cloud->size() << std::endl;
 
-	pcl::PointIndicesPtr cloud_indices(new pcl::PointIndices);
-	pcl::PointIndicesPtr ground_indices(new pcl::PointIndices);
 
 
 	// 测试临时屏蔽
 	// 提取地面
 	GroundExtract::FindGroundIndices(src_cloud, cloud_indices, ground_indices);
 	if (ground_indices->indices.size() < 2)
-		return EXIT_SUCCESS;
- 	Common::OutlierRemoval(src_cloud, Setting::ins().liQunDian_.radius, Setting::ins().liQunDian_.count, ground_indices);
-	
+		return EXIT_FAILURE;
+	//Common::OutlierRemoval(src_cloud, Setting::ins().liQunDian_.radius, Setting::ins().liQunDian_.count, ground_indices);
+	std::cout << "end FindGroundIndices." << std::endl;
 
 	Common::ExtractCloud(src_cloud, ground_indices, ground_cloud);
 	pct::io::save_las_off(ground_cloud, groundLas);
+	std::cout << "end FindGroundIndices save_las_off." << std::endl;
 
-	
 	CgalClassif::classif(groundLas);
+	std::cout << "end classif." <<  std::endl;
+
 	pct::io::Load_las_off(src_cloud, groundLas);
 	Common::ExtractColorCloud(src_cloud);
 	Common::GenerateCloud(src_cloud, ground_indices);
@@ -96,31 +102,31 @@ int main(int argc, char *argv[])
 				continue;
 			}
 			pt = &binghai_cloud->at(ptIndex);
-			
- 			pt->r = 255;
- 			pt->g = 255;
- 			pt->b = 255;
- 			if (bh.flag_yb.get()[i].get()[j] != 0)
- 			{
- 				pt->r = 255;
- 				pt->g = 0;
- 				pt->b = 0;
- 			}
- 
- 			if (bh.flag_cx.get()[i].get()[j] != 0)
- 			{
- 				pt->r = 0;
- 				pt->g = 0;
-				pt->b = 255;
- 			}
 
-// 			if (ptIndex < 2050)
-// 			{
-// 				pt = &binghai_cloud->at(ptIndex);
-// 				pt->r = 255;
-// 				pt->g = 0;
-// 				pt->b = 0;
-// 			}
+			pt->r = 255;
+			pt->g = 255;
+			pt->b = 255;
+			if (bh.flag_yb.get()[i].get()[j] != 0)
+			{
+				pt->r = 255;
+				pt->g = 0;
+				pt->b = 0;
+			}
+
+			if (bh.flag_cx.get()[i].get()[j] != 0)
+			{
+				pt->r = 0;
+				pt->g = 0;
+				pt->b = 255;
+			}
+
+			// 			if (ptIndex < 2050)
+			// 			{
+			// 				pt = &binghai_cloud->at(ptIndex);
+			// 				pt->r = 255;
+			// 				pt->g = 0;
+			// 				pt->b = 0;
+			// 			}
 
 			ptIndex++;
 		}
@@ -132,3 +138,4 @@ int main(int argc, char *argv[])
 	w.exec();
 	return a.exec();
 }
+
